80c45201583c959e0ad3607d8ea341d12c66f2fc
Michael <billyston@Michaels-MacBook-Pro.local>
2023-11-08 00:24:03 +0000

    WelcomeState modified.

--- app/States/Welcome/WelcomeState.php
+++ app/States/Welcome/WelcomeState.php
@@ -17,14 +17,14 @@ final class WelcomeState
         // Customer exist and status is active
         if (!$customer == null && data_get(target: $customer, key: 'status') === 'active') {
             return ResponseBuilder::ussdResourcesResponseBuilder(
-                message: "Welcome Back\n1). Some Menu 2\n2). Some Menu 2\n3) Some Menu 3",
+                message: "Menu\n1. Some Menu 2\n2. Some Menu 2\n3. Some Menu 3",
                 session_id: data_get(target: $request, key: 'SessionId'),
             );
         }
 
         // Return the registration menu
         return ResponseBuilder::ussdResourcesResponseBuilder(
-            message: "Welcome! Ssb\n1). Register now\n2). Terms & Conditions\n0) Exit",
+            message: "Menu\n1. Register now\n2. Terms & Conditions\n0. Exit",
             session_id: data_get(target: $request, key: 'SessionId'),
         );
     }
