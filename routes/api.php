1b273de5b3b42160a9ed287f3dc498f4988700cb
Michael <billyston@Michaels-MacBook-Pro.local>
2023-11-08 00:13:45 +0000

    Route modified.

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,15 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\V1\UssdController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1/ussd')
     ->as('v1.ussd:')
     ->group(base_path('routes/v1/routes.php'));
+
+Route::post(
+    uri: 'api/main',
+    action: UssdController::class
+)->name(name: 'main');
+
