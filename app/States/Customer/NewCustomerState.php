f7283356ba646d3d5858cbea8c29fd6b6a13c830
Michael <billyston@Michaels-MacBook-Pro.local>
2023-11-10 07:57:40 +0000

    NewCustomerState invalid input handled. Pending attempts count.

--- app/States/Customer/NewCustomerState.php
+++ app/States/Customer/NewCustomerState.php
@@ -3,8 +3,6 @@
 namespace App\States\Customer;
 
 use App\Common\ResponseBuilder;
-use Domain\Customer\Actions\Common\GetCustomerAction;
-use Domain\Shared\Action\CreateSessionAction;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class NewCustomerState
@@ -12,8 +10,20 @@ class NewCustomerState
     public static function execute(
         array $request,
     ): JsonResponse {
+        // Create the expected input arrays
+        $options = ['1', '2'];
+
+        // Check if use input is in the array
+        if (in_array(data_get(target: $request, key: 'Message'), haystack: $options)) {
+            return ResponseBuilder::ussdResourcesResponseBuilder(
+                message: "Enter First Name\n",
+                session_id: data_get(target: $request, key: 'SessionId'),
+            );
+        }
+
+        // The customer input is invalid
         return ResponseBuilder::ussdResourcesResponseBuilder(
-            message: "Enter First Name\n",
+            message: "Invalid input. Try again\n1 Menu\n1. Register now\n2. Terms & Conditions\n0. Exit",
             session_id: data_get(target: $request, key: 'SessionId'),
         );
     }
