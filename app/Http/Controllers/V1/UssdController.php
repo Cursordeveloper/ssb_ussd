da1d5eb636b7b1e529bcfd563a75ef2b3451641a
Michael <billyston@Michaels-MacBook-Pro.local>
2023-11-25 09:13:09 +0000

    UssdController modified.

--- app/Http/Controllers/V1/UssdController.php
+++ app/Http/Controllers/V1/UssdController.php
@@ -16,7 +16,12 @@ final class UssdController extends Controller
         // Create new instance of the ussd service (HubtelUssdService)
         $ussd_service = new HubtelUssdService($request);
 
-//        return ResponseBuilder::terminateResponseBuilder(session_id: $ussd_service->session_id);
+        // Define numbers to be allowed (Testing purposes)
+        $permitted = ['0244294960', '233244294960', '0242662430', '233242662430'];
+
+        if (! in_array($ussd_service->msisdn, $permitted)) {
+            return ResponseBuilder::terminateResponseBuilder(session_id: $ussd_service->session_id);
+        }
 
         // Return the response
         return StateManager::execute($ussd_service);
