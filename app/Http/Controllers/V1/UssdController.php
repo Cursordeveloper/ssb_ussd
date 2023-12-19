932d9e1e2484ba8da4de93ff7c16a29981c70a1a
Michael <billyston@Michaels-MacBook-Pro.local>
2023-12-19 09:28:05 +0000

    CreateNewSusu: Output messages modified.

--- app/Http/Controllers/V1/UssdController.php
+++ app/Http/Controllers/V1/UssdController.php
@@ -17,10 +17,10 @@ final class UssdController extends Controller
         $session_data = new HubtelUssdService($request);
 
         // Define numbers to be allowed (Testing purposes)
-        $permitted = ['0244294960', '233244294960', '0242662430', '233242662430', '0246936458', '233246936458'];
-        if (! in_array($session_data->msisdn, $permitted)) {
-            return ResponseBuilder::terminateResponseBuilder(session_id: $session_data->session_id);
-        }
+        //        $permitted = ['0244294960', '233244294960', '0242662430', '233242662430', '0246936458', '233246936458'];
+        //        if (! in_array($session_data->msisdn, $permitted)) {
+        //            return ResponseBuilder::terminateResponseBuilder(session_id: $session_data->session_id);
+        //        }
 
         // Return the response
         return StateManager::execute($session_data);
