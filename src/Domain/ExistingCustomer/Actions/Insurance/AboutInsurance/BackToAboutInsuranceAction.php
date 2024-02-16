d2b0c9bc3d0eadf92c51aeb3c94f6b076637d469
Michael <billyston@Michaels-MacBook-Pro.local>
2024-02-16 04:09:34 +0000

    AboutInsuranceState: All menus and states done. Pending real contents.

--- src/Domain/ExistingCustomer/Actions/Insurance/AboutInsurance/BackToAboutInsuranceAction.php
+++ src/Domain/ExistingCustomer/Actions/Insurance/AboutInsurance/BackToAboutInsuranceAction.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Domain\ExistingCustomer\Actions\Insurance\AboutInsurance;
 
-use App\Menus\ExistingCustomer\Loan\AboutLoans\AboutLoansMenu;
-use App\States\ExistingCustomer\Loans\AboutLoans\AboutLoansState;
+use App\Menus\ExistingCustomer\Insurance\AboutInsurance\AboutInsuranceMenu;
+use App\States\ExistingCustomer\Insurance\AboutInsurance\AboutInsuranceState;
 use Domain\Shared\Action\Session\SessionInputUpdateAction;
 use Domain\Shared\Action\Session\SessionUpdateAction;
 use Domain\Shared\Models\Session\Session;
@@ -16,7 +16,7 @@ final class BackToAboutInsuranceAction
     public static function execute(Session $session, $session_data): JsonResponse
     {
         // Define the return state and menu
-        $state = ['class' => new AboutLoansState, 'menu' => new AboutLoansMenu];
+        $state = ['class' => new AboutInsuranceState, 'menu' => new AboutInsuranceMenu];
 
         // Update the customer session action
         SessionUpdateAction::execute(session: $session, state: class_basename($state['class']), session_data: $session_data);
