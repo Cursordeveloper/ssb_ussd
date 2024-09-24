82cc4bb1522a71c6afe151aa60bdceb261fd51e3
Michael <billyston@gmail.com>
2024-09-24 11:01:24 +0000

    PersonalSusuAccount: Collection(summary, pause) features cleaned up

--- src/Domain/Susu/PersonalSusu/States/Collection/Summary/PersonalSusuCollectionSummaryState.php
+++ src/Domain/Susu/PersonalSusu/States/Collection/Summary/PersonalSusuCollectionSummaryState.php
@@ -13,13 +13,9 @@ final class PersonalSusuCollectionSummaryState
 {
     public static function execute(Session $session, $service_data): JsonResponse
     {
-        // Get the process flow array from the customer session (user inputs)
-        $user_inputs = json_decode($session->user_inputs, associative: true);
-
         // Evaluate the process flow and execute the corresponding action
         return match (true) {
-            ! array_key_exists(key: 'approval', array: $user_inputs) => PersonalSusuCollectionSummaryApprovalAction::execute(session: $session, service_data: $service_data),
-
+            ! array_key_exists(key: 'approval', array: $session->userInputs()) => PersonalSusuCollectionSummaryApprovalAction::execute(session: $session, service_data: $service_data),
             default => GeneralMenu::systemErrorNotification(session: $session),
         };
     }
