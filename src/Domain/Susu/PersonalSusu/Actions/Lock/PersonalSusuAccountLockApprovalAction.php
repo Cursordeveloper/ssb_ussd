7da041b5cc1861cd99852fd393b3388485676f74
Michael <billyston@gmail.com>
2024-09-24 23:01:13 +0000

    PersonalSusuAccount: Lock features cleaned up

--- src/Domain/Susu/PersonalSusu/Actions/Lock/PersonalSusuAccountLockApprovalAction.php
+++ src/Domain/Susu/PersonalSusu/Actions/Lock/PersonalSusuAccountLockApprovalAction.php
@@ -4,41 +4,59 @@ declare(strict_types=1);
 
 namespace Domain\Susu\PersonalSusu\Actions\Lock;
 
+use App\Services\Susu\Data\PersonalSusu\Lock\SusuServicePersonalSusuAccountLockCancellationData;
 use App\Services\Susu\Requests\PersonalSusu\Lock\SusuServicePersonalSusuAccountLockApprovalRequest;
-use Domain\Shared\Action\Session\SessionInputUpdateAction;
+use App\Services\Susu\Requests\PersonalSusu\Lock\SusuServicePersonalSusuAccountLockCancellationRequest;
+use Domain\Shared\Action\General\GeneralValidation;
 use Domain\Shared\Data\Common\PinApprovalData;
 use Domain\Shared\Menus\General\GeneralMenu;
 use Domain\Shared\Models\Session\Session;
-use Domain\User\Customer\Actions\Common\GetCustomerAction;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 final class PersonalSusuAccountLockApprovalAction
 {
     public static function execute(Session $session, $service_data): JsonResponse
     {
-        // Update the user inputs (steps)
-        SessionInputUpdateAction::updateUserInputs(session: $session, user_input: ['approval' => true]);
+        // Execute and return the response (menu)
+        return match (true) {
+            $service_data->user_input === '2' => self::cancelExecution(session: $session),
+            GeneralValidation::pinLengthValid($service_data->user_input) === false => GeneralMenu::pinLengthMenu(session: $session),
+            default => self::approvalExecution(session: $session, service_data: $service_data)
+        };
+    }
 
-        // Get the process flow array from the customer session (user inputs)
-        $user_inputs = json_decode($session->user_inputs, associative: true);
-
-        // Get the customer
-        $customer = GetCustomerAction::execute($session->phone_number);
-
-        // Execute the SusuServicePersonalSusuAccountLockApprovalRequest HTTP request and return the response
-        $balance = (new SusuServicePersonalSusuAccountLockApprovalRequest)->execute(
-            customer: $customer,
+    public static function approvalExecution(Session $session, $service_data): JsonResponse
+    {
+        // Execute the SusuServicePersonalSusuAccountLockApprovalRequest and return the response
+        $response = (new SusuServicePersonalSusuAccountLockApprovalRequest)->execute(
+            customer: $session->customer,
             data: PinApprovalData::toArray($service_data->user_input),
-            susu_resource: data_get(target: $user_inputs, key: 'susu_account.attributes.resource_id'),
-            lock_resource: data_get(target: $user_inputs, key: 'account_lock_data.resource'),
+            susu_resource: data_get(target: $session->userInputs(), key: 'susu_account.attributes.resource_id'),
+            lock_resource: data_get(target: $session->userInputs(), key: 'account_lock_data.resource_id'),
         );
 
-        // Terminate session if $get_balance request status is false
-        if (data_get(target: $balance, key: 'code') !== 200) {
-            return GeneralMenu::invalidInput(session: $session);
-        }
+        // Process response and return menu
+        return match (true) {
+            data_get($response, key: 'code') === 200 => GeneralMenu::requestNotification(session: $session),
+            data_get($response, key: 'code') === 401 => GeneralMenu::incorrectPinMenu(session: $session),
+            default => GeneralMenu::systemErrorNotification(session: $session)
+        };
+    }
 
-        // Return the requestNotification and terminate the session
-        return GeneralMenu::requestNotification(session: $session);
+    public static function cancelExecution(Session $session): JsonResponse
+    {
+        // Execute the SusuServicePersonalSusuAccountLockCancellationRequest HTTP request
+        $response = (new SusuServicePersonalSusuAccountLockCancellationRequest)->execute(
+            customer: $session->customer,
+            data: SusuServicePersonalSusuAccountLockCancellationData::toArray(),
+            susu_resource: data_get(target: $session->userInputs(), key: 'susu_account.attributes.resource_id'),
+            lock_resource: data_get(target: $session->userInputs(), key: 'account_lock_data.resource_id'),
+        );
+
+        // Process response and return menu
+        return match (true) {
+            data_get($response, key: 'code') === 200 => GeneralMenu::infoNotification(session: $session, message: data_get(target: $response, key: 'description')),
+            default => GeneralMenu::systemErrorNotification(session: $session)
+        };
     }
 }
--- src/Domain/Susu/PersonalSusu/Menus/Lock/PersonalSusuAccountLockMenu.php
+++ src/Domain/Susu/PersonalSusu/Menus/Lock/PersonalSusuAccountLockMenu.php
@@ -13,53 +13,29 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 final class PersonalSusuAccountLockMenu
 {
-    public static function mainMenu($session): JsonResponse
+    public static function mainMenu(Session $session): JsonResponse
     {
-        // Get the process flow array from the customer session (user_inputs, user_data)
-        $user_inputs = json_decode($session->user_inputs, associative: true);
-
-        // Match statement to determine the menu to return
         return match (true) {
-            data_get(target: $user_inputs, key: 'susu_account.attributes.settlement_status') === 'locked' => self::settlementLockedMenu(session: $session),
+            data_get(target: $session->userInputs(), key: 'susu_account.attributes.settlement_status') === 'locked' => self::settlementLockedMenu(session: $session),
             default => self::durationMenu(session: $session)
         };
     }
 
-    public static function settlementLockedMenu($session): JsonResponse
+    public static function settlementLockedMenu(Session $session): JsonResponse
     {
-        // Get the process flow array from the customer session (user_inputs, user_data)
-        $user_inputs = json_decode($session->user_inputs, associative: true);
-
-        // Return the menu for the susu_scheme
         return ResponseBuilder::infoResponseBuilder(
-            message: 'Settlements on this account has been locked on: '.data_get(target: $user_inputs, key: 'susu_account.included.account_lock.attributes.locked_at').'. Will be unlocked on: '.data_get(target: $user_inputs, key: 'susu_account.included.account_lock.attributes.unlocked_at'),
+            message: 'Settlements on this account has been locked on: '.data_get(target: $session->userInputs(), key: 'susu_account.included.account_lock.attributes.locked_at').'. Will be unlocked on: '.data_get(target: $session->userInputs(), key: 'susu_account.included.account_lock.attributes.unlocked_at'),
             session_id: $session->session_id,
         );
     }
 
-    public static function durationMenu($session): JsonResponse
+    public static function durationMenu(Session $session): JsonResponse
     {
         // Execute the duration
         (new GetSusuDurationsAction)::execute(session: $session);
 
-        // Get the durations from the session->user_data
-        $durations = json_decode($session->user_data, associative: true);
-
-        // Return the menu for the susu_scheme
         return ResponseBuilder::ussdResourcesResponseBuilder(
-            message: "Choose duration\n".SusuResources::formatDurationsForMenu(durations: $durations['durations']),
-            session_id: $session->session_id,
-        );
-    }
-
-    public static function invalidDurationMenu($session): JsonResponse
-    {
-        // Get the durations from the session->user_data
-        $durations = json_decode($session->user_data, associative: true);
-
-        // Return the invalidDurationMenu
-        return ResponseBuilder::ussdResourcesResponseBuilder(
-            message: "Invalid duration\n".SusuResources::formatDurationsForMenu(durations: $durations['durations']),
+            message: "Choose duration\n".SusuResources::formatDurationsForMenu(durations: $session->userData()['durations']),
             session_id: $session->session_id,
         );
     }
@@ -67,7 +43,7 @@ final class PersonalSusuAccountLockMenu
     public static function narrationMenu(Session $session, array $response): JsonResponse
     {
         return ResponseBuilder::ussdResourcesResponseBuilder(
-            message: 'Lock date: '.Carbon::parse(data_get(target: $response, key: 'data.attributes.locked_at'))->isoFormat(format: 'MM/DD/YYYY').'. Resume date:'.Carbon::parse(data_get(target: $response, key: 'data.attributes.unlocked_at'))->isoFormat(format: 'MM/DD/YYYY').'. Enter pin to confirm or 2 to Cancel.',
+            message: 'Lock date: '.Carbon::parse(data_get(target: $response, key: 'data.attributes.locked_at'))->isoFormat(format: 'MM/DD/YYYY').'. Resume date:'.Carbon::parse(data_get(target: $response, key: 'data.attributes.unlocked_at'))->isoFormat(format: 'MM/DD/YYYY').'. Enter PIN to confirm or 2 to Cancel.',
             session_id: $session->session_id,
         );
     }
