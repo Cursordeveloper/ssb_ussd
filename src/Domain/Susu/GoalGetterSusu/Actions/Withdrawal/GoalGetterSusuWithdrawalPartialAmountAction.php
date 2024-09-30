913b80f3867cf8cdb5126a452f3747024fb37d35
Michael <billyston@gmail.com>
2024-09-30 16:45:25 +0000

    SusuAccounts: Flexy susu payment modified

--- src/Domain/Susu/GoalGetterSusu/Actions/Withdrawal/GoalGetterSusuWithdrawalPartialAmountAction.php
+++ src/Domain/Susu/GoalGetterSusu/Actions/Withdrawal/GoalGetterSusuWithdrawalPartialAmountAction.php
@@ -4,14 +4,26 @@ declare(strict_types=1);
 
 namespace Domain\Susu\GoalGetterSusu\Actions\Withdrawal;
 
+use Domain\Shared\Action\General\SusuValidationAction;
 use Domain\Shared\Action\Session\SessionInputUpdateAction;
 use Domain\Shared\Menus\General\GeneralMenu;
+use Domain\Shared\Menus\General\SusuValidationMenu;
 use Domain\Shared\Models\Session\Session;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 final class GoalGetterSusuWithdrawalPartialAmountAction
 {
     public static function execute(Session $session, $service_data): JsonResponse
+    {
+        // Validate and process the user_input
+        return match (true) {
+            SusuValidationAction::isNumericValid($service_data->user_input) === false => SusuValidationMenu::isNumericMenu(session: $session),
+
+            default => self::actionExecution(session: $session, service_data: $service_data)
+        };
+    }
+
+    public static function actionExecution(Session $session, $service_data): JsonResponse
     {
         // Update the user inputs (steps)
         SessionInputUpdateAction::updateUserInputs(session: $session, user_input: ['withdrawal_amount' => $service_data->user_input]);
